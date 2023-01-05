f950c719dde681cfb5b26a5dad6c6d73945bc09b
Yeferson Marcano <yefelex10@gmail.com>
2023-01-05 11:29:45 -0300

    Profile

--- screens/index.js
+++ screens/index.js
@@ -1,9 +1,11 @@
 import Home from "./Home";
 import BookDetail from "./BookDetail"
 import Login from "./Login/Index"
+import Profile from "./Profile/Index"
 
 export {
     Home,
     BookDetail,
-    Login
+    Login,
+    Profile
 };
