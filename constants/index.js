71d6bb9cad229522e2a07d1c91c4f3a635530963
Yeferson Marcano <yefelex10@gmail.com>
2023-01-05 11:29:03 -0300

    Dummy Data

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,6 @@
 import icons from "./icons";
 import images from "./images";
+import dummyData from "./dummy";
 import theme, { COLORS, SIZES, FONTS } from "./theme";
 
-export { icons, images, theme, COLORS, SIZES, FONTS };
+export { icons, images, theme, COLORS, SIZES, FONTS, dummyData };
