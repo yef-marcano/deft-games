2bf44a81cc46a9bff8aef1a33bd918d00c59521d
Yeferson Marcano <yefelex10@gmail.com>
2023-01-14 16:29:44 -0300

    Update images.js

--- constants/images.js
+++ constants/images.js
@@ -3,10 +3,12 @@ export const theMetropolist = require("../assets/images/uno.jpg");
 export const theTinyDragon = require("../assets/images/clashroyal.jpg");
 export const underland = require("../assets/images/underland.jpg");
 export const logo = require("../assets/images/logo.png");
+export const splash = require("../assets/splash1.png");
 
 export default {
     otherWordsForHome,
     theMetropolist,
     theTinyDragon,
-    logo
+    logo,
+    splash
 }
