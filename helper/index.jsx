4accc671edc312d2d5397cf184fff527c52e0fc3
Yeferson Marcano <yefelex10@gmail.com>
2023-01-14 16:30:37 -0300

    Update index.jsx

--- helper/index.jsx
+++
@@ -1,6 +0,0 @@
-import {imgCrypto} from "./imgCrypto";
-import {sharedRss} from "./sharedRss";
-import {walletSelector} from "./walletSelector";
-import formats from "./formats";
-
-export { imgCrypto, sharedRss, walletSelector, formats };
